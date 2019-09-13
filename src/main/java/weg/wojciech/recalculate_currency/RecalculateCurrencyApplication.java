89952431b83ea27b1e94dda4fa23b5459320c038
WojciechWeg <wojtek.weg@gmail.com>
2019-09-13 17:30:48 +0200

    init

---
+++ src/main/java/weg/wojciech/recalculate_currency/RecalculateCurrencyApplication.java
@@ -0,0 +1,13 @@
+package weg.wojciech.recalculate_currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecalculateCurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecalculateCurrencyApplication.class, args);
+    }
+
+}
